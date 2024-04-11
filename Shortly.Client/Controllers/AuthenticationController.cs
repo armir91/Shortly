9dd33a74c937c789d0608e1ce81c80a24d144b1e
Armir <65112930+armir91@users.noreply.github.com>
2024-04-11 17:41:18 +0200

    Handling Login Form Submission

--- Shortly.Client/Controllers/AuthenticationController.cs
+++ Shortly.Client/Controllers/AuthenticationController.cs
@@ -19,7 +19,7 @@ namespace Shortly.Client.Controllers
 
         public IActionResult LoginSubmitted(LoginVM loginVM)
         {
-            return View();
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult Register()
