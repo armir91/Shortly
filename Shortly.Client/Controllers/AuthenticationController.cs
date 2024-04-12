13a860ef44c3f48d5c88c1aaefccc29831bb3c67
Armir <65112930+armir91@users.noreply.github.com>
2024-04-12 12:15:33 +0200

    REgister form data validation

--- Shortly.Client/Controllers/AuthenticationController.cs
+++ Shortly.Client/Controllers/AuthenticationController.cs
@@ -26,7 +26,14 @@ namespace Shortly.Client.Controllers
 
         public IActionResult Register()
         {
-            return View();
+            return View(new RegisterVM());
+        }
+
+        public IActionResult RegisterUser(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View("Register", registerVM);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- Shortly.Client/Data/ViewModels/RegisterVM.cs
+++ Shortly.Client/Data/ViewModels/RegisterVM.cs
@@ -2,14 +2,21 @@
 
 namespace Shortly.Client.Data.ViewModels
 {
-    public class LoginVM
+    public class RegisterVM
     {
-        [Required(ErrorMessage="Email is required")]
+        [Required(ErrorMessage = "The Full Name is required!")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
         [RegularExpression(@"^\S+@\S+\.\S+$", ErrorMessage = "Invalid email address")]
         public string EmailAddress { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [MinLength(5, ErrorMessage = "Password must be at least 5 characters long!")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [Compare("Password", ErrorMessage = "Confirm password does not match with the password entered!")]
+        public string ConfirmPassword { get; set; }
     }
 }
