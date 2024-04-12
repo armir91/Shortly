7e007271dbc806240c505f60820d73de7de6f05e
Armir <65112930+armir91@users.noreply.github.com>
2024-04-12 15:47:28 +0200

    Added first migration and created the database

--- Shortly.Client/Program.cs
+++ Shortly.Client/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllersWithViews();
 //Configure the AppDbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString(""));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
