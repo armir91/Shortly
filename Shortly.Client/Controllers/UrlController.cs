d577701ecfe0a73009e270af9f6932f8686a31c4
Armir <65112930+armir91@users.noreply.github.com>
2024-04-15 10:10:08 +0200

    get all links from DB

--- Shortly.Client/Controllers/UrlController.cs
+++ Shortly.Client/Controllers/UrlController.cs
@@ -1,34 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shortly.Client.Data.ViewModels;
+using Shortly.Data;
 
 namespace Shortly.Client.Controllers
 {
     public class UrlController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public UrlController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            //Fake DB data
-            var allUrls = new List<GetUrlVM>()
-            {
-                new GetUrlVM()
-                {
-                    Id = 1,
-                    OriginalLink = "https://link1.com",
-                    ShortLink = "sh1",
-                    NrOfClicks = 1,
-                    UserId = 1
-                },
-                new GetUrlVM()
-                {
-                    Id = 2,
-                    OriginalLink = "https://link2.com",
-                    ShortLink = "sh2",
-                    NrOfClicks = 2,
-                    UserId = 2
-                }
-            };
 
-            return View(allUrls);
+            var urls = _context.Urls
+                .Select(url => new GetUrlVM()
+                    {
+                        Id = url.Id,
+                        OriginalLink = url.OriginalLink,
+                        ShortLink = url.ShortLink,
+                        NrOfClicks = url.NrOfClicks,
+                        UserId = url.UserId,
+                    })
+                .ToList();
+
+            return View(urls);
         }
 
         public IActionResult Create()
