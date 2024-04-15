b67abbe3f639c1a1303612e2f17f99ee5f7b35f9
Armir <65112930+armir91@users.noreply.github.com>
2024-04-15 10:51:02 +0200

    used TempData to display success message

--- Shortly.Client/Controllers/HomeController.cs
+++ Shortly.Client/Controllers/HomeController.cs
@@ -1,15 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Shortly.Client.Data.ViewModels;
+using Shortly.Data;
+using Shortly.Data.Models;
 
 namespace Shortly.Client.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -23,7 +27,34 @@ namespace Shortly.Client.Controllers
            // validate the model
            if(!ModelState.IsValid) return View("Index", postUrlVM);
 
+            var newUrl = new Url()
+            {
+               OriginalLink = postUrlVM.Url,
+               ShortLink = GenerateShortUrl(3),
+               NrOfClicks = 0,
+               UserId = null,
+               DateCreated = DateTime.UtcNow
+            };
+
+            _context.Urls.Add(newUrl);
+            _context.SaveChanges();
+
+            TempData["Message"] = $"Your url has been shortened successfully to {newUrl.ShortLink}";
+
            return RedirectToAction("Index");
+        }
+
+        private string GenerateShortUrl(int length)
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            return new string(
+                Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)])
+                .ToArray()
+                );
+
         }
     }
 }
