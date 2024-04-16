689b66a7f61e93cb990699dbd985157373773b2a
Armir <65112930+armir91@users.noreply.github.com>
2024-04-16 14:46:12 +0200

    Implemented methods for Url and User Service

--- Shortly.Data/Services/UrlsService.cs
+++ Shortly.Data/Services/UrlsService.cs
@@ -14,7 +14,9 @@ namespace Shortly.Data.Services
 
         public Url GetById(int id)
         {
-            throw new NotImplementedException();
+            var url = _context.Urls.FirstOrDefault(x => x.Id == id);
+
+            return url;
         }
 
         public List<Url> GetUrls()
@@ -29,17 +31,36 @@ namespace Shortly.Data.Services
 
         public Url Add(Url url)
         {
-            throw new NotImplementedException();
+            _context.Urls.Add(url);
+            _context.SaveChanges();
+
+            return url;
         }
 
         public Url Update(int id, Url url)
         {
-            throw new NotImplementedException();
+            var urlDb = _context.Urls.FirstOrDefault(x => x.Id == id);
+            if (urlDb != null)
+            {
+                urlDb.OriginalLink = url.OriginalLink;
+                urlDb.ShortLink = url.ShortLink;
+                urlDb.DateUpdated = url.DateUpdated;
+
+                _context.Update(urlDb);
+                _context.SaveChanges();
+            }
+
+            return urlDb;
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            var urlDb = _context.Urls.FirstOrDefault(x => x.Id == id);
+            if (urlDb != null)
+            {
+                _context.Urls.Remove(urlDb);
+                _context.SaveChanges();
+            }
         }
     }
 }
