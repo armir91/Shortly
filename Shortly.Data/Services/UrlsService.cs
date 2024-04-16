3fe4ce38af63a17829105d7a927318b4f11aeecb
Armir <65112930+armir91@users.noreply.github.com>
2024-04-16 14:32:41 +0200

    Added Services and their respective methods(User and Url)

---
+++ Shortly.Data/Services/UrlsService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shortly.Data.Models;
+
+namespace Shortly.Data.Services
+{
+    public class UrlsService : IUrlsService
+    {
+        private readonly AppDbContext _context;
+
+        public UrlsService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Url GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Url> GetUrls()
+        {
+            var allUrls = _context
+                .Urls
+                .Include(user => user.User)
+                .ToList();
+
+            return allUrls;
+        }
+
+        public Url Add(Url url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Url Update(int id, Url url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Shortly.Data/Services/UsersService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shortly.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortly.Data.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly AppDbContext _context;
+
+        public UsersService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetUsers()
+        {
+            var allUSers = _context
+                .Users
+                .Include(url => url.Urls)
+                .ToList();
+
+            return allUSers;
+        }
+
+        public User Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(int id, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
